c04a22b5186ee53a579900041d71a77fe00f507b
Chris Carlon <chris.carlon25@gmail.com>
2013-01-22 22:30:21 +0000

    Simplify runner structure

--- lib/metrognome/runner.rb
+++ lib/metrognome/runner.rb
@@ -1,7 +1,7 @@
 module Metrognome
   class Runner
     class << self
-      # TODO pull out reaper_interval into config
+      # TODO pull out REAPER_INTERVAL into config
       attr_accessor :running, :reaper_last
       REAPER_INTERVAL = 1
 
@@ -9,11 +9,7 @@ module Metrognome
         setup
 
         loop do
-          if @_sigterm
-            running.each { |t| t.kill }
-            reap
-            break
-          end
+          break if @_sigterm
           reap if Time.now - reaper_last > REAPER_INTERVAL
 
           Metrognome::Registrar.registered.each do |sched|
@@ -21,16 +17,16 @@ module Metrognome
               running << Thread.new do
                 sched.call_task
 
-                # DB connections will no longer be closed automatically, so
-                # close it manually here
+                # DB connections are not closed automatically when threads exit
                 ActiveRecord::Base.connection.close
               end
             end
           end
-          # TODO GCD sleep instead of 1
           sleep 1
         end
 
+        running.each { |t| t.kill }
+        reap
         Metrognome::Registrar.registered.each { |s| File.delete s.lock }
       end
 
